4fdf59536f0999e3372f7ff3ec2353cb727d4293
aldoBangun <punk.aldo68@gmail.com>
2020-02-26 09:38:56 +0800

    initial commit

---
+++ js/script.js
@@ -0,0 +1,76 @@
+const start = document.getElementsByTagName('button')[0];
+const menu = document.getElementsByClassName('main-menu')[0];
+const field = document.getElementsByClassName('field')[0];
+start.addEventListener('click',function(){
+    // start.innerHTML = 'RESTART';
+    menu.remove();
+    const result = document.getElementsByClassName('result')[0];
+    const pScore = document.querySelector('.player-score h1');
+    const cScore = document.querySelector('.com-score h1');
+    const round = document.getElementsByClassName('round')[0];
+    pScore.innerHTML = '0';
+    cScore.innerHTML = '0';
+    round.innerHTML = 'Round 1';
+
+    const getCom = function(){
+        const com = Math.random();
+        if(com<0.34) return 'semut';
+        if(com>=0.34 && com<0.67) return 'orang';
+        return 'gajah';
+    }
+
+    const getResult = function(com,player){
+        if(com == player) return 'draw';
+        if(com == 'gajah') return (player == 'orang') ? 'kalah' : 'menang';
+        if(com == 'orang') return (player == 'semut') ? 'kalah' : 'menang';
+        return (player == 'gajah') ? 'kalah' : 'menang';
+    }
+
+    const player = document.querySelectorAll('.player-side li img');
+    let scoreCom = 0;
+    let scorePlayer = 0;
+    let rnd = 1;
+
+    player.forEach(function(play){
+        play.addEventListener('click',function(){
+            play.style.filter = 'drop-shadow(0 0 15px #f77)';
+            setTimeout(function(){
+                play.style.filter = 'none';
+            },2000);
+            const imgCom = document.querySelector('.com-side img');
+            const com = getCom();
+            const player = play.className;
+            imgCom.setAttribute('src',`img/${com}.png`);
+            const res = getResult(com,player);
+            if(res == 'menang') {
+                scorePlayer += 3;
+                scoreCom += 0;
+                result.style.backgroundColor = '#9f9';
+            } else if ( res == 'kalah') {
+                scorePlayer += 0;
+                scoreCom += 3;
+                result.style.backgroundColor = '#f99';
+            } else {
+                scorePlayer +=1;
+                scoreCom +=1;
+                result.style.backgroundColor = '#99f';
+            }
+            rnd++;
+            if(rnd>10){
+                round.innerHTML = 'final score';
+                setTimeout(function(){
+                    field.remove();
+                },500);
+            } else {
+                setTimeout(function(){
+                    round.innerHTML = 'round ' + rnd;
+                },1000);
+            }
+            result.innerHTML = res + '!';
+            setTimeout(function(){
+                pScore.innerHTML = scorePlayer;
+                cScore.innerHTML = scoreCom;
+            },500);
+        });
+    });
+});
